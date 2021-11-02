6d1cca844491d1db64313847171acf2e0b3a0221
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2021-11-02 16:44:05 +0700

    implement: lumen-generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -96,6 +96,7 @@ $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 // Add this line
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
